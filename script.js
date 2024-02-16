3874d241171b5d15e5a50e657cd1ead4173b2e21
Julia <julia.naczke007@gmail.com>
2024-02-16 16:57:19 +0100

    section 2 and 3

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ window.addEventListener('scroll', function(e){
         target[index].style.transform = 'translate3d(0px, '+pos+'px, 0px)';
     }
 });
+
+function rangeSlider() {
+    let slider = document.querySelectorAll(".range-slider");
+    let range = document.querySelectorAll(".range-slider__range");
+    let value = document.querySelectorAll(".range-slider__value");
+  
+    slider.forEach((currentSlider) => {
+      value.forEach((currentValue) => {
+        let val = currentValue.previousElementSibling.getAttribute("value");
+        currentValue.innerText = val;
+      });
+  
+      range.forEach((elem) => {
+        elem.addEventListener("input", (eventArgs) => {
+          elem.nextElementSibling.innerText = eventArgs.target.value;
+        });
+      });
+    });
+  }
+  rangeSlider();
